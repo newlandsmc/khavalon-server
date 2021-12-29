4d02ea4369d695d74113af5b9025e341e8919d4e
bdagenais2 <braydon.dagenais@gmail.com>
2021-12-29 07:40:48 +0100

    Leanining into the competitive multiplayer

---
+++ lwc-cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/var/minecraft/mcrcon/mcrcon -H localhost -P 25575 -p a8Haw76Oaj2e9WJ2b 'lwc admin expire 180 days'
+
+sleep 120
+
+/var/minecraft/mcrcon/mrcon -H localhost -P 25575 -p a8Haw76Oaj2e9WJ2b 'lwc admin cleanup'
+
